87776d7f9bf5e50a956dbe09f35314a659f10b9d
Muhammad Daffa Ariq Fadilah <114640665+Muhammad-Daffa-Ariq-Fadilah@users.noreply.github.com>
2022-10-26 11:26:01 +0700

    Update M.Daffa Ariq.F_1C Informatika_Tugas 4 No 3.cpp

--- M.Daffa Ariq.F_1C Informatika_Tugas 4 No 3.cpp
+++ M.Daffa Ariq.F_1C Informatika_Tugas 4 No 3.cpp
@@ -6,6 +6,8 @@ int main(){
     cout << "Masukkan tahun: ";
     cin >> tahun;
     if(tahun%400==0){
+        cout << tahun << " Merupakan tahun kabisat";
+    }else if(tahun%100==0){
         cout << tahun << " Bukan tahun kabisat";
     }else if(tahun%4==0){
         cout << tahun << " Merupakan tahun kabisat";
